0ad357e15325edb8089155754889e33669ba0a8a
杨冬青 <18210189817@qq.com>
2020-06-10 16:02:02 +0800

    init

---
+++ SwiftUI-Demo/SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by 杨冬青 on 2020/6/10.
+//  Copyright © 2020 杨冬青. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
