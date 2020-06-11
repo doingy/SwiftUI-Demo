9dd7678124356798a523a0d3d0f993711ba678a2
杨冬青 <18210189817@qq.com>
2020-06-11 18:59:52 +0800

    feat: PokeMaster Init

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by 杨冬青 on 2020/6/11.
+//  Copyright © 2020 杨冬青. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
